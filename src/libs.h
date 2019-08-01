b89dabf4d3ce0a049f20ed178b5fb128edb0c73c
Andriy Makukha <andriy.makukha@gmail.com>
2019-08-02 03:50:10 +0800

    libs: copy, preposterous, release

--- src/libs.h
+++ src/libs.h
@@ -10,6 +10,8 @@
 // TODO: this library should be reworked to either:
 //  - do all operations completely in memory
 //  - write files only when the size if getting too big
+//  - write a unit test for this (starting with log2)
+//  - check whether stack[sp+2] have correct offsets (especially when r3 was present)
 
 #include "tmgc.h"
 
@@ -32,13 +34,14 @@ extern void errcom(const char* msg);
 tword flag;
 tword stats[9];
 tword useful;
-tword afi;
+FILE* afi;
 FILE* afout;
 tword datadr;
 tptr  hblk;                         // must remain here - pointer to free header
-tword frlist[FRSIZE];               // NOTE: originally hblk immediately precedes frlist
+                                    // NOTE: originally hblk immediately precedes both frlist and strbuf
+tword frlist[FRSIZE];               //
 #define frend   (frlist + FRSIZE)
-char  strbuf[HSZ];                  // NOTE: originally hblk immediately precedes strbuf, strbuf overlaps with frlist
+char  strbuf[HSZ];                  // NOTE: originally strbuf overlaps with frlist
 #define headers strbuf              // Synonym
 #define strend  (strbuf + HSZ)
 tword nchar;
@@ -52,15 +55,15 @@ char b1[NUMB*BUFSIZE];              // Buffers data
 // Structs (from alloc3.s)
 
 // each block has a four-word header:
-//	W - write ptr (also used as link ptr in frlist)
-//	R - read ptr
-//	A - pointer to head of data
-//	L - ptr to (end+1) of data
+//      W - write ptr (also used as link ptr in frlist)
+//      R - read ptr
+//      A - pointer to head of data
+//      L - ptr to (end+1) of data. (End of allocated range.)
 typedef struct sblock {
-    tptr w; 
-    tptr r;
-    tptr a;
-    tptr l;
+    tword w; 
+    tword r;
+    tword a;
+    tword l;
 } sblock_t;
 
 // Function declarations
@@ -69,6 +72,8 @@ void allocate();
 void alterword();
 void clean();
 void copy();
+int ilog2(tuword x);
+void preposterous();
 void release();
 void reset();
 void seekchar();
@@ -78,6 +83,15 @@ void whead();
 
 // Function definitions
 
+// Description:
+//	allocate a new block
+// Parameters:
+//	requested size in bytes in r0
+//	(convert to words, adjust for header, round up
+//	to a power of two)
+// Return:
+//	pointer to header of allocated block returned in r1
+//	r0 is preserved
 void allocate() {
     // TODO
 }
@@ -87,101 +101,134 @@ void alterword() {
 }
 
 // Description:
-//	routine to wc a buffer
+//      routine to wc a buffer.
+//      (Subroutine of wc())
 // Parameters:
 //      bn  - buffer index
 //      buf - beginning of buffer
-void clean(tword bn, char* buf) {
+inline void clean(tword bn, char* buf) {
     fseek(afout, b1s[bn], SEEK_SET);
     fwrite(buf, 1, BUFSIZE, afout);
     w1[bn] = 0;
 }
 
 // Description:
-//	routine to copy the contents of one string
-//	to another.
+//      routine to copy the contents of one string to another.
 // Parameters:
-//	r0 - source
-//	r1 - dest
+//      r0 - source (r0 is preserved)
+//      r1 - dest
 // Return:
-//	on return, r1 points to the new string and should
-//	be saved.  r0 is preserved.
+//      r1 points to the new string and should be saved.
 void copy() {
     PUSH(r0);
     PUSH(r2);
-    //PUSH(r3);
+    tword r3;
     wc();
-l1:
-    reset();
-    // <-- CONT
-//1:	jsr	pc,reset
-//	mov	w(r0),r2
-//	sub	a(r0),r2	/W-A
-//	mov	l(r1),r3
-//	sub	a(r1),r3	/L-A
-//	cmp	r2,r3
-//	ble	1f
-//	jsr	pc,release
-//	mov	r2,r0
-//	jsr	pc,allocate
-//	mov	4(sp),r0
-///
-//1:	mov	a(r1),w(r1)	/rewind w pointer
-//	mov	a(r0),-(sp)
-///
-//4:	mov	(sp),0f
-//	mov	afi,r0
-//	sys	seek;0:.. ;0	/set input pointer
-//	cmp	r2,$512.
-//	ble	2f
-//	mov	$512.,r3	/# output this time
-//	mov	r3,0f
-//	mov	r3,3f
-//	add	r3,(sp)
-//	sub	r3,r2	/# left to output
-//	br	1f
-///
-//2:	mov	r2,0f
-//	mov	r2,3f
-//	mov	r2,r3
-//	clr	r2
-///
-//1:	mov	afi,r0
-//	sys	read;b1;0:..
-//	bes	bad
-//	cmp	r0,r3
-//	bne	bad
-//	mov	afout,r0
-//	mov	(r1),0f
-//	add	r3,(r1)
-//	sys	seek;0:.. ;0
-//	sys	write;b1;3:..
-//	bes	bad
-//	tst	r2
-//	bgt	4b
-//	tst	(sp)+
-//	mov	4(sp),r0	/restore r0
-////
-////	fix up read ptr of new string
-////
-//1:	mov	r(r0),r2
-//	sub	a(r0),r2
-//	add	a(r1),r2
-//	mov	r2,r(r1)
-////
-////	restore and return
-////
-//	mov	(sp)+,r3
-//	mov	(sp)+,r2
-//	mov	(sp)+,r0
-//	rts	pc
-///
-//bad:
-//        errcom("error on copy");
+
+    sblock_t* _r0 = (sblock_t*)r0;      // Convenience cast
+    sblock_t* _r1 = (sblock_t*)r1;
+    r2 = _r0->w - _r0->a;               // W-A: number of bytes to be copied from source
+    r3 = _r1->l - _r1->a;               // L-A: number of bytes allocated in in dest
+    if (r2 > r3) {
+        release();                      // Free block pointed by r1
+        r0 = r2;                        // New size
+        allocate();
+        r0 = stack[sp+1];               // restore r0 (source)
+        _r0 = (sblock_t*)r0;
+    }
+
+    // rewind w pointer
+    _r1 = (sblock_t*)r1;
+    _r1->w = _r1->a;
+    tword roff = _r0->a;                // Read offset
+    do {
+        // set input pointer
+        fseek(afi, roff, SEEK_SET);     // TODO: it's unnecessary to have seeks in loop
+        if (r2 > 512) { 
+            r3 = 512;                   // # output this time
+            roff += r3;
+        } else
+            r3 = r2;
+        r2 -= r3;                       // # left to output
+        int ret = fread(b1, 1, r3, afi);
+        if (ret < r3) goto bad;
+        fseek(afout, _r1->w, SEEK_SET);
+        _r1->w += r3;
+        ret = fwrite(b1, 1, r3, afout);
+        if (ret < r3) goto bad;
+    } while (r2 > 0);
+
+    // fix up read ptr of new string
+    _r1->r = _r0->r - _r0->a + _r1->a;
+
+    // restore and return
+    r2 = POP();
+    r0 = POP();
+    return;
+bad:
+    errcom("error on copy");
 }
 
+// Description:
+//      routine to find integer part of log2(x)
+//      (Generalized to tword.)
+int ilog2(tuword x) {
+    int cnt = 0;
+    for (tuword r = 1; r; r <<= 1, cnt++)
+        if (r > x) break;
+    return --cnt>=0 ? cnt : 0;      // Don't return negative result for r0==0
+}
+
+// Description:
+//      routine to discover whether r1 points to
+//      a plausible header - to avoid ruination.
+//      r1 is preserved 
+// Return:
+//      r0 gets a suitable index for frlist
+void preposterous() {
+    if ((tptr)r1 < (tptr)strbuf) goto botch;
+    if ((tptr)r1 >= (tptr)strend) goto botch;
+    sblock_t* _r1 = (sblock_t*)r1;
+    if (_r1->a < HEADSZ) goto botch;    // A
+    if (_r1->l > datadr) goto botch;    // L
+    r0 = _r1->l - _r1->a;
+    PUSH(r0);
+    r0 = ilog2(r0);
+    r0 <<= 1;       // what's the point here? Doesn't make sense. Did they mean +1? TODO
+    if ((1<<r0) != stack[sp]) goto botch;
+    stack[sp] = r0;
+    r0 = frlist[r0];
+    while (r0 != 0) {
+        r0 += (tword)hblk;          // TODO: what is the meaning of this??? DANGEROUS
+        if (r0 == r1) goto botch;
+        r0 = *(tptr)r0;
+    }
+    r0 = POP();
+    return;
+botch:
+    errcom("error in allocator.");
+}
+
+// Description:
+//      here to release a block
+// Parameters:
+//      pointer to block in r1
 void release() {
-    // TODO
+    // discover that this is a plausible pointer
+    // and get a suitable index for frlist into r0.
+    PUSH(r0);
+    preposterous();                     
+
+    // find free list index and link block to that entry
+    stats[1]++; 
+    sblock_t* _r1 = (sblock_t*)r1;      // Convenience casting
+    _r1->w = frlist[r0];
+    _r1->r = 0;
+    r1 = (tuword)r1 - (tuword)hblk;     // TODO: what's the meaning of this??? DANGEROUS
+    frlist[r0] = r1;
+    r1 = 0;                 // self-defense
+    // whead()
+    r0 = POP();
 }
 
 void reset() {
@@ -194,30 +241,34 @@ void reset() {
     flag = 0;
 }
 
+// Description:
+//      routine to move the read pointer of a string to the
+//      relative position indicated by r0.  the string is
+//      extended if necessary - there is no error return.
 // Parameters:
 //      r0 - position
-//      r1 - ...
+//      r1 - ... (The string.)
 void seekchar() {
     PUSH(r1);
     PUSH(r0);
-l1:
-    r0 = stack[sp];
-    r0 += (tuword)((sblock_t*)r1)->a;
-    if ((tptr)r0 > ((sblock_t*)r1)->l) goto l3;
-    ((sblock_t*)r1)->r = (tptr)r0;
-    if ((tptr)r0 < ((sblock_t*)r1)->w) goto f1;
-    ((sblock_t*)r1)->w = (tptr)r0;
-    goto f1;
-l3:
-    r0 = stack[sp];
-    allocate();
-    r0 = stack[sp+1];
-    copy();
-    swap();
-    release();
-    r1 = stack[sp+1];
-    goto l1;
-f1:
+    do {
+        r0 = stack[sp];         // Restore r0 (position)
+        r0 += (tuword)((sblock_t*)r1)->a;
+        if (r0 > ((sblock_t*)r1)->l) {
+            r0 = stack[sp];     // Requested allocation size for string r1
+            allocate();
+            r0 = stack[sp+1];
+            copy();             // Copy the string to newly allocated block currently in r1
+            swap();             // r0 <- new block, r1 <- old block
+            release();          // Free old block pointed by r1
+            r1 = stack[sp+1];   // Restore r1 (string)
+        } else {
+            ((sblock_t*)r1)->r = r0;
+            if (r0 >= ((sblock_t*)r1)->w)
+                ((sblock_t*)r1)->w = r0;
+            break;
+        }
+    } while(1);
     r0 = POP();
     r1 = POP();
 }
@@ -246,6 +297,6 @@ void wc() {
 // Description:
 //      write header block to the output file
 void whead() {
-    fseek(afout, 0L, SEEK_SET);
+    fseek(afout, 0, SEEK_SET);
     fwrite(hblk, 1, HSZ, afout);
 }
