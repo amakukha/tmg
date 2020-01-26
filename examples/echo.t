410ca039718421226c0075484fa8221b67e1c49e
Andriy Makukha <andriy.makukha@gmail.com>
2020-01-26 22:58:00 +0800

    Simpler echo example

--- examples/echo.t
+++ examples/echo.t
@@ -1,7 +1,7 @@
 /* Unix TMG program which copies entire input to output */
 
 prog:   parse(echo);
-echo:   smark ignore(none) any(ascii) string(ascii) scopy done = { 2 };
-done:   = {};
+echo:   smark ignore(none) any(ascii) string(ascii) scopy done;
+done:   ;
 none:   <<>>;
 ascii:  !<<>>;
--- test/05_echo/tmgl.h
+++ test/05_echo/tmgl.h
@@ -1,51 +1,41 @@
 // Port of the Unix compiler-compiler TMG to C99.
 // Based on the original PDP-11 assembly code by M. D. McIlroy.
-// (c) 2019, Andriy Makukha, 2-clause BSD License.
+// (c) 2020, Andriy Makukha, 2-clause BSD License.
 
 #define SRC_LANGUAGE "echo"
 
 const tword labels[];
 
-// Driving table for a sample program (see examples/echo.t)
+// Driving table for the program
 
 tword start[] = {
-#define __1        (tword)&labels[0]
+#define __1	(tword)&labels[0]
 	1 + __1,
 // __1:
-#define __prog     (tword)&start[1]
+#define __prog	(tword)&start[1]
 	1 + (tword)&parse,
-#define __echo     (tword)&labels[1]
+#define __echo	(tword)&labels[1]
 	__echo,
-#undef _1
-#define _1         (tword)&start[3]
-	1 + (tword)&_tp,
-	2,
 // __echo:
 	(tword)&smark,
 	(tword)&ignore,
-#define __none     (tword)&labels[2]
+#define __none	(tword)&labels[2]
 	__none,
 	(tword)&any,
-#define __ascii    (tword)&labels[3]
+#define __ascii	(tword)&labels[3]
 	__ascii,
 	(tword)&string,
 	__ascii,
 	(tword)&scopy,
-#define __done     (tword)&labels[4]
-	__done,
-	1 + (tword)&trans,
-	1 + _1,
-#undef _1
-#define _1         (tword)&start[16]
-	1 + (tword)&generate,
+#define __done	(tword)&labels[4]
+	1 + __done,
 // __done:
-	1 + (tword)&trans,
-	1 + _1,
+	1 + (tword)&succ,
 // __none:
 	1,
 // __ascii:
 	2,
-#define __classtab (tword)&start[21]
+#define __classtab	(tword)&start[15]
 	0,
 	2,
 	2,
@@ -174,10 +164,10 @@ tword start[] = {
 	2,
 	2,
 	2,
-#define _pn        (tword)&start[149]
+#define _pn	(tword)&start[143]
 	1 + (tword)&_pxs,
 	(tword)'\n',
-#define _tn        (tword)&start[151]
+#define _tn	(tword)&start[145]
 	1 + (tword)&_txs,
 	(tword)'\n',
 };
@@ -185,22 +175,20 @@ tword start[] = {
 // Global label addresses inside the driving table
 
 const tword labels[] = {
-	(tword)&start[1],       // __1
-	(tword)&start[5],       // __echo
-	(tword)&start[19],      // __none
-	(tword)&start[20],      // __ascii
-	(tword)&start[17],      // __done
+	(tword)&start[1],	// __1
+	(tword)&start[3],	// __echo
+	(tword)&start[13],	// __none
+	(tword)&start[14],	// __ascii
+	(tword)&start[12],	// __done
 };
 
 // Used builtins:
 //   _pxs
-//   _tp
 //   _txs
 //   any
-//   generate
 //   ignore
 //   parse
 //   scopy
 //   smark
 //   string
-//   trans
+//   succ
