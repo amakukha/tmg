eb42bdbb1bc3e734a90e15d4ff087b3bb750b2d0
Andriy Makukha <andriy.makukha@gmail.com>
2020-03-09 00:35:44 +0800

    Adding $HOME/bin to $PATH

--- install.sh
+++ install.sh
@@ -26,4 +26,38 @@ if [ -f "$cmnd" ]; then
     [[ ! $YESNO =~ ^[Yy]$ ]] && echo "ERROR: $cmnd already exists" && exit 1
 fi
 cp tmg.sh "$cmnd"
-echo "TMG installed into \$HOME/bin; make sure it's in \$PATH"
+echo "TMG installed into \$HOME/bin"
+
+# Add $HOME/bin to $PATH
+absent () {
+    # Returns 0 (true) if string $2 is not found in file "$HOME/$1"
+    [[ ! -f "$HOME/$1" ]] && return 0
+    if grep -q "$2" "$HOME/$1"; then return 1; fi
+    return 0
+}
+
+amend () {
+    # Add string $2 to file "$HOME/$1" after user's confirmation
+    read -p "Do you want to add \$HOME/bin into \$PATH in ~/$1? [yN] " -s -r -n 1 YESNO
+    echo "$YESNO"
+    [[ ! $YESNO =~ ^[Yy]$ ]] && return 1
+    echo "$2" >> "$HOME/$1"
+    echo "Please, run the following command to use TMG:"
+    echo "    source ~/$1"
+    return 0
+}
+
+if [[ $PATH != *"$HOME/bin:"* ]] && [[ $PATH != *"$HOME/bin/:"* ]]; then
+    # $HOME/bin is not found in $PATH
+    exp='export PATH="$HOME/bin:$PATH"'
+    if absent ".bash_profile" "$exp" && absent ".bashrc" "$exp" && absent ".profile" "$exp"; then
+        # $HOME/bin seems to be not added to $PATH in the common shell startup files
+        err=0
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            amend ".bash_profile" "$exp" || amend ".bashrc" "$exp" || amend ".profile" "$exp" || err=1
+        else
+            amend ".bashrc" "$exp" || amend ".bash_profile" "$exp" || amend ".profile" "$exp" || err=1
+        fi
+        [ $err -ne 0 ] && echo "Add \$HOME/bin into \$PATH"
+    fi
+fi
